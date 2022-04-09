20274b7994c525199fc478e6b9503a5d6ce8ee4b
忍者哈特利 <ks0dcongra@gmail.com>
2022-04-10 01:57:13 +0800

    feat: add update todo function

--- app.js
+++ app.js
@@ -34,13 +34,13 @@ app.get('/', (req, res) => {
     .catch(error => console.error(error))// 錯誤處理
 })
 
-app.get('/todo/new', (req, res) => {
+app.get('/todos/new', (req, res) => {
   return res.render('new')
 })
 
-app.post('/todos', (req, res) =>{
+app.post('/todos', (req, res) => {
   const name = req.body.name
-  
+
   //第一種寫法
   // const todo = new Todo({name})
   // return todo.save()
@@ -48,7 +48,7 @@ app.post('/todos', (req, res) =>{
   //   .catch(error => console.log(error))
 
   //第二種寫法
-  return Todo.create({name})
+  return Todo.create({ name })
     .then(() => res.redirect('/'))
     .catch(error => console.log(error))
 })
@@ -56,3 +56,34 @@ app.post('/todos', (req, res) =>{
 app.listen(port, () => {
   console.log('App is running on http://localhost:3000');
 })
+
+app.get('/todos/:id', (req, res) => {
+  // 動態參數，可以用 req.params 取出
+  const id = req.params.id
+  return Todo.findById(id)
+    .lean()
+    .then((todo) => res.render('detail', { todo }))
+    .catch(error => console.log(error))
+})
+
+app.get('/todos/:id/edit', (req, res) => {
+  // 動態參數，可以用 req.params 取出
+  const id = req.params.id
+  return Todo.findById(id)
+    .lean()
+    .then((todo) => res.render('edit', { todo }))
+    .catch(error => console.log(error))
+})
+
+app.post('/todos/:id/edit', (req, res) => {
+  const id = req.params.id
+  const name = req.body.name
+  return Todo.findById(id)
+    .then(todo => {
+      todo.name = name
+      return todo.save()
+    })
+    .then(() => res.redirect(`/todos/${id}`))
+    .catch(error => console.log(error))
+})
+
