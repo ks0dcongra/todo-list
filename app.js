261a3ecef0753116a0cd71c50fefd6e0bb29bed5
忍者哈特利 <ks0dcongra@gmail.com>
2022-04-09 20:50:46 +0800

    feat: modify index.hbs

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose') // 載入 mongoose
 const mongodb_url = require('./mongodb_url')
 const exphbs = require('express-handlebars')
 const port = 3000
+const Todo = require('./models/todo')
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
@@ -21,7 +22,10 @@ db.once('open', () => {
 })
 
 app.get('/', (req, res) => {
-  res.render('index')
+  Todo.find()// 取出 Todo model 裡的所有資料
+    .lean()// 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
+    .then(todos => res.render('index', { todos: todos }))// 將資料傳給 index 樣板
+    .catch(error => console.error(error))// 錯誤處理
 })
 
 
