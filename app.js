5f2c02ca440bd6ed6676cc3d5b3ddfee86c076ba
忍者哈特利 <ks0dcongra@gmail.com>
2022-04-09 20:28:33 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose') // 載入 mongoose
-
 const mongodb_url = require('./mongodb_url')
-
+const exphbs = require('express-handlebars')
 const port = 3000
+
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
 
 mongoose.connect(mongodb_url(), { useNewUrlParser: true, useUnifiedTopology: true })
 // 取得資料庫連線狀態
@@ -19,11 +21,11 @@ db.once('open', () => {
 })
 
 app.get('/', (req, res) => {
-  res.send('hello world')
+  res.render('index')
 })
+
+
 
 app.listen(port, () => {
   console.log('App is running on http://localhost:3000');
 })
-
-require('dotenv').config({ path: 'ENV_FILENAME' });
