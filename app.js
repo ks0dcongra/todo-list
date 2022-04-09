385617624e2c012a7942380131fd42110609fbf0
忍者哈特利 <ks0dcongra@gmail.com>
2022-04-09 22:40:37 +0800

    feat: add create todo function

--- app.js
+++ app.js
@@ -5,11 +5,8 @@ const mongodb_url = require('./mongodb_url')
 const exphbs = require('express-handlebars')
 const port = 3000
 const Todo = require('./models/todo')
-
-app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
-app.set('view engine', 'hbs')
-
-mongoose.connect(mongodb_url(), { useNewUrlParser: true, useUnifiedTopology: true })
+// 引用 body-parser
+const bodyParser = require('body-parser')
 // 取得資料庫連線狀態
 const db = mongoose.connection
 // 連線異常
@@ -21,6 +18,15 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
+// 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
+app.use(bodyParser.urlencoded({ extended: true }))
+
+mongoose.connect(mongodb_url(), { useNewUrlParser: true, useUnifiedTopology: true })
+
+
 app.get('/', (req, res) => {
   Todo.find()// 取出 Todo model 裡的所有資料
     .lean()// 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
@@ -28,7 +34,24 @@ app.get('/', (req, res) => {
     .catch(error => console.error(error))// 錯誤處理
 })
 
+app.get('/todo/new', (req, res) => {
+  return res.render('new')
+})
 
+app.post('/todos', (req, res) =>{
+  const name = req.body.name
+  
+  //第一種寫法
+  // const todo = new Todo({name})
+  // return todo.save()
+  //   .then(() => res.redirect('/'))
+  //   .catch(error => console.log(error))
+
+  //第二種寫法
+  return Todo.create({name})
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
 
 app.listen(port, () => {
   console.log('App is running on http://localhost:3000');
