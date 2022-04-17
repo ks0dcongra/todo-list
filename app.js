625b1fc92e3473631d3b7c7364816e24f327429e
忍者哈特利 <ks0dcongra@gmail.com>
2022-04-17 15:50:57 +0800

    feat: add sort function

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ mongoose.connect(mongodb_url(), { useNewUrlParser: true, useUnifiedTopology: tru
 app.get('/', (req, res) => {
   Todo.find()// 取出 Todo model 裡的所有資料
     .lean()// 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
+    .sort({ _id: 'asc' }) // 新增這裡：根據 _id 升冪排序
     .then(todos => res.render('index', { todos: todos }))// 將資料傳給 index 樣板
     .catch(error => console.error(error))// 錯誤處理
 })
