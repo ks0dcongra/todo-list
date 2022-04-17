f81e7773828934a520d37b5ce25bd7dd6b6a3e44
忍者哈特利 <ks0dcongra@gmail.com>
2022-04-17 17:39:10 +0800

    refactor: modify express routes

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ const bodyParser = require('body-parser')
 // 載入 method-override
 const methodOverride = require('method-override')
 // 取得資料庫連線狀態
+// 引用路由器
+const routes = require('./routes')
+
 const db = mongoose.connection
 // 連線異常
 db.on('error', () => {
@@ -19,8 +22,6 @@ db.on('error', () => {
 db.once('open', () => {
   console.log('mongodb connected!')
 })
-
-
 
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
@@ -33,76 +34,85 @@ app.use(bodyParser.urlencoded({ extended: true }))
 // 設定每一筆請求都會透過 methodOverride 進行前置處理
 app.use(methodOverride('_method'))
 
+
+// 將 request 導入路由器
+app.use(routes)
+
+
 // mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.connect(mongodb_url(), { useNewUrlParser: true, useUnifiedTopology: true })
 
-
-app.get('/', (req, res) => {
-  Todo.find()// 取出 Todo model 裡的所有資料
-    .lean()// 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
-    .sort({ _id: 'asc' }) // 新增這裡：根據 _id 升冪排序
-    .then(todos => res.render('index', { todos: todos }))// 將資料傳給 index 樣板
-    .catch(error => console.error(error))// 錯誤處理
-})
-
-app.get('/todos/new', (req, res) => {
-  return res.render('new')
-})
-
-app.post('/todos', (req, res) => {
-  const name = req.body.name
-
-  //第一種寫法
-  // const todo = new Todo({name})
-  // return todo.save()
-  //   .then(() => res.redirect('/'))
-  //   .catch(error => console.log(error))
-
-  //第二種寫法
-  return Todo.create({ name })
-    .then(() => res.redirect('/'))
-    .catch(error => console.log(error))
-})
 
 app.listen(port, () => {
   console.log('App is running on http://localhost:3000');
 })
 
-app.get('/todos/:id', (req, res) => {
-  // 動態參數，可以用 req.params 取出
-  const id = req.params.id
-  return Todo.findById(id)
-    .lean()
-    .then((todo) => res.render('detail', { todo }))
-    .catch(error => console.log(error))
-})
 
-app.get('/todos/:id/edit', (req, res) => {
-  // 動態參數，可以用 req.params 取出
-  const id = req.params.id
-  return Todo.findById(id)
-    .lean()
-    .then((todo) => res.render('edit', { todo }))
-    .catch(error => console.log(error))
-})
+// // Route加進來 刪掉
+// app.get('/', (req, res) => {
+//   Todo.find()// 取出 Todo model 裡的所有資料
+//     .lean()// 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
+//     .sort({ _id: 'asc' }) // 新增這裡：根據 _id 升冪排序
+//     .then(todos => res.render('index', { todos: todos }))// 將資料傳給 index 樣板
+//     .catch(error => console.error(error))// 錯誤處理
+// })
 
-app.put('/todos/:id', (req, res) => {
-  const id = req.params.id
-  const { name, isDone } = req.body
-  return Todo.findById(id)
-    .then(todo => {
-      todo.name = name
-      todo.isDone = isDone === 'on'
-      return todo.save()
-    })
-    .then(() => res.redirect(`/todos/${id}`))
-    .catch(error => console.log(error))
-})
+// app.get('/todos/new', (req, res) => {
+//   return res.render('new')
+// })
 
-app.delete('/todos/:id', (req, res) => {
-  const id = req.params.id
-  return Todo.findById(id)
-    .then(todo => todo.remove())
-    .then(() => res.redirect('/'))
-    .catch(error => console.log(error))
-})
+// app.post('/todos', (req, res) => {
+//   const name = req.body.name
+
+//   //第一種寫法
+//   // const todo = new Todo({name})
+//   // return todo.save()
+//   //   .then(() => res.redirect('/'))
+//   //   .catch(error => console.log(error))
+
+//   //第二種寫法
+//   return Todo.create({ name })
+//     .then(() => res.redirect('/'))
+//     .catch(error => console.log(error))
+// })
+
+
+
+// app.get('/todos/:id', (req, res) => {
+//   // 動態參數，可以用 req.params 取出
+//   const id = req.params.id
+//   return Todo.findById(id)
+//     .lean()
+//     .then((todo) => res.render('detail', { todo }))
+//     .catch(error => console.log(error))
+// })
+
+// app.get('/todos/:id/edit', (req, res) => {
+//   // 動態參數，可以用 req.params 取出
+//   const id = req.params.id
+//   return Todo.findById(id)
+//     .lean()
+//     .then((todo) => res.render('edit', { todo }))
+//     .catch(error => console.log(error))
+// })
+
+// app.put('/todos/:id', (req, res) => {
+//   const id = req.params.id
+//   const { name, isDone } = req.body
+//   return Todo.findById(id)
+//     .then(todo => {
+//       todo.name = name
+//       todo.isDone = isDone === 'on'
+//       return todo.save()
+//     })
+//     .then(() => res.redirect(`/todos/${id}`))
+//     .catch(error => console.log(error))
+// })
+
+// app.delete('/todos/:id', (req, res) => {
+//   const id = req.params.id
+//   return Todo.findById(id)
+//     .then(todo => todo.remove())
+//     .then(() => res.redirect('/'))
+//     .catch(error => console.log(error))
+// })
