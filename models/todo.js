09549689319ee931b9b8ea0139ab229a3f5bfb6a
忍者哈特利 <ks0dcongra@gmail.com>
2022-04-17 15:47:58 +0800

    feat: add todo-list checked function

--- models/todo.js
+++ models/todo.js
@@ -1,9 +1,14 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
+
 const todoSchema = new Schema({
   name: {
     type: String,
     required: true
+  },
+  isDone: {
+    type: Boolean,
+    default: false  // 預設完成狀態為 false
   }
 })
 
