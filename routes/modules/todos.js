24b9dbea48391b86e422ec40d6d94c3276131a32
忍者哈特利 <ks0dcongra@gmail.com>
2022-04-20 20:46:58 +0800

    feat: add multiple create function

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -4,12 +4,20 @@ const Todo = require('../../models/todo')
 router.get('/new', (req, res) => {
   return res.render('new')
 })
+
+// 用1,1,1來create多個物件
 router.post('/', (req, res) => {
-  const name = req.body.name
-  return Todo.create({ name })
-    .then(() => res.redirect('/'))
-    .catch(error => console.log(error))
+  const todos = req.body.name.split(',').map(todo => ({ name: todo }))
+  Todo.insertMany(todos).then(() => {
+    return res.redirect('/')
+  })
+  // 改良前create單個物件
+  // const name = req.body.name
+  // return Todo.create({ name })
+  //   .then(() => res.redirect('/'))
+  //   .catch(error => console.log(error))
 })
+
 router.get('/:id', (req, res) => {
   const id = req.params.id
   return Todo.findById(id)
